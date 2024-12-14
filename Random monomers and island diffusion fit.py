a6f2ef68fb1fe193c96c8451e5e354e5017e48e0
Hugo ALEXANDRE <hugoalexandre127@gmail.com>
2024-12-14 16:03:35 +0100

    Update Random monomers and island diffusion fit.py

--- Random monomers and island diffusion fit.py
+++ Random monomers and island diffusion fit.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation, FFMpegWriter
+import matplotlib.animation as animation
 from scipy.ndimage import label
 from scipy.integrate import odeint
 from scipy.optimize import curve_fit
